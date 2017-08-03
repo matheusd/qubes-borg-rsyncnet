d8508921d79aee2677cc4d7819b2c6e8844634bc
Matheus Degiovani <math@matheusd.com>
2017-08-03 15:20:42 -0300

    Improving README.

--- template/install.sh
+++ template/install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+# Make the necessary dirs
 mkdir -p ~/.ssh/config.d
+mkdir -p ~/bin
+
+# Copy and assemble the ~/.ssh/config file
 cp bakserver ~/.ssh/config.d
 cat ~/.ssh/config.d/* > ~/.ssh/config
-mkdir -p ~/bin
+
+# Copy the borgbak utility script
 cp borgbak ~/bin
 chmod 700 ~/bin/borgbak
 
+# Install the backup crontab
 crontab bak.crontab
